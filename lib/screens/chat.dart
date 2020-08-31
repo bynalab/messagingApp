a890b0c0d1a0ae544d51673bebd351f75a331d24
malgwi <malgwisunday@gmail.com>
2020-08-31 13:31:36 +0100

    chat screen

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat';
@@ -10,6 +11,81 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.grey,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.phone,
+                color: Colors.grey,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.image,
+              size: 50,
+              color: Color.fromRGBO(2, 104, 251, 1),
+            ),
+            title: Text(
+              'Images',
+              style: GoogleFonts.roboto(color: Colors.grey),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.insert_drive_file,
+              size: 50,
+              color: Color.fromRGBO(174, 86, 251, 1),
+            ),
+            title: Text(
+              'File',
+              style: GoogleFonts.roboto(color: Colors.grey),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              size: 50,
+              color: Color.fromRGBO(234, 75, 51, 1),
+            ),
+            title: Text(
+              'Contact',
+              style: GoogleFonts.roboto(color: Colors.grey),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.location_on,
+              size: 50,
+              color: Color.fromRGBO(232, 76, 163, 1),
+            ),
+            title: Text(
+              'Location',
+              style: GoogleFonts.roboto(color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
